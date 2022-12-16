5c7ef76144018f15a7eeefaab884870602b1d40b
RajeshwariPatil1100 <rajeshwaripatil1100@gmail.com>
2022-12-16 19:42:44 +0530

    [Rajeshwari] Maximum int at First Position

--- GenericsDemo/Program.cs
+++ GenericsDemo/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaximum
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int max = FindMaximum.FindIntMax(123, 456, 789);
+            Console.WriteLine(max);
+        }
+    }
+}
